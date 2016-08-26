16dfd4b64c52fb710e9635c4ef21b9eaceca5d6c
Angie Smith <avksmit2@gmail.com>
2016-08-26 16:38:09 -0700

    add styling and pics, everything appears functioning and all specs met.

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,13 @@ function Customer(custName, phone) {
 this.custName = custName;
 this.phone = phone;
 }
+
 function Pizza(size, toppings) {
 this.toppings = toppings;
 this.size = size;
 this.price = 0;
 }
-function Store(custName) {
-this.custName = custName;
-this.pizzaNum = 0;
-// this.size = "";
-// this.toppings = [];
-this.price = 0;
-}
+
 Pizza.prototype.getPizzaPrice = function() {
   if (this.size === "Small") {
     this.price = 7.00;
@@ -28,22 +23,20 @@ Pizza.prototype.getPizzaPrice = function() {
   return this.price += this.toppings.length * 1.50;
   store.price += this.price;
 }
+var getToppings = function() {
+  var selectedToppings = []
+  var toppings = document.getElementsByClassName('toppings');
+  for (var i=0;i<toppings.length;i++) {
+    if (toppings[i].checked) {
+      selectedToppings.push(toppings[i].id);
+    }
+  };
+  return selectedToppings;
+}
+
 //Front-end
 $(document).ready(function() {
-  var store;
-  var num = num++
-  var pizza1;
-
-  var getToppings = function() {
-    var selectedToppings = []
-    var toppings = document.getElementsByClassName('toppings');
-    for (var i=0;i<toppings.length;i++) {
-      if (toppings[i].checked) {
-        selectedToppings.push(toppings[i].id);
-      }
-    };
-    return selectedToppings;
-  }
+  var pizza;
 
   $("form#orderForm").submit(function(event) {
     var custName = $("#custName").val();
@@ -53,15 +46,10 @@ $(document).ready(function() {
     event.preventDefault();
 
     newCustomer = new Customer(custName, phone);
-    store = new Store(custName);
-    pizza1 = new Pizza(size, getToppings());
+    pizza = new Pizza(size, getToppings());
 
-    $("#priceSum #size").text("Size selected: " + pizza1.size)
-    pizza1.toppings.forEach(function(topping) {
-      $("#priceSum #toppings").append("<li>" + topping + "</li");
-    })
-    var price = pizza1.getPizzaPrice();
-    console.log(price);
-    $("#priceSum #price").text("Total Price: " + pizza1.getPizzaPrice());
+    var price = pizza.getPizzaPrice();
+    $("#priceSum #price").text("Total Price: " + pizza.getPizzaPrice());
+    $(".share").show();
   });
 });
